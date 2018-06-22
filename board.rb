6b686666d507e3a923f14a4c78c9d750f77d0404
Jeffrey Chan <jefrayc@gmail.com>
2018-06-21 18:21:59 -0700

    level-11 done

--- board.rb
+++ board.rb
@@ -1,5 +1,5 @@
 require_relative "tile"
-
+require "byebug"
 class Board
   def self.empty_grid
     Array.new(9) do
@@ -10,7 +10,8 @@ class Board
   def self.from_file(filename)
     rows = File.readlines(filename).map(&:chomp)
     tiles = rows.map do |row|
-      nums = row.split(" ").map { |char| Integer(char) }
+
+      nums = row.split("").map { |char| Integer(char) }
       nums.map { |num| Tile.new(num) }
     end
 
